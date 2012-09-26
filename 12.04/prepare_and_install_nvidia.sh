f8f74e74c5dcd347b31cc78c5d047bd0e34142b3
Bram77 <bramvano@gmail.com>
2012-09-26 22:33:23 +0200

    Fixed adding repository

--- 12.04/prepare_and_install_nvidia.sh
+++ 12.04/prepare_and_install_nvidia.sh
@@ -3,18 +3,6 @@
 VIDEO_DRIVER="nvidia-current"
 VIDEO_MANUFACTURER="NVIDIA"
 
-rotdash2()
-{
-p=$1
-while [ -d /proc/$p ]
-do
-echo -n '/^H' ; sleep 0.05
-echo -n '-^H' ; sleep 0.05
-echo -n '\^H' ; sleep 0.05
-echo -n '|^H' ; sleep 0.05
-done
-}
-
 echo ""
 echo "-----------"
 echo ""
@@ -22,59 +10,42 @@ echo "Please enter your password to start Ubuntu preparation and XBMC installati
 echo "Your computer will restart automatically once the process has been completed."
 
 ## Fix locale bug
-sudo echo "LC_MESSAGES=\"C\"" >> /etc/environment &
-rotdash2 $!
-sudo echo "LC_ALL=\"en_US.UTF-8\"" >> /etc/environment &
-rotdash2 $!
+sudo echo "LC_MESSAGES=\"C\"" >> /etc/environment
+sudo echo "LC_ALL=\"en_US.UTF-8\"" >> /etc/environment
 
 echo "-----"
 echo "Locale environment bug successfully fixed"
 echo "Adding Wsnipex xbmc-xvba-testing PPA..."
 
 ## Add XBMCbuntu testing ppa to sources.list and reload repositories
-sudo echo "" >> /etc/apt/sources.list &
-rotdash2 $!
-sudo echo "## XBMCbuntu testing PPA" >> /etc/apt/sources.list &
-rotdash2 $!
-sudo echo "deb http://ppa.launchpad.net/wsnipex/xbmc-xvba-testing/ubuntu quantal main" >> /etc/apt/sources.list &
-rotdash2 $!
-sudo echo "deb-src http://ppa.launchpad.net/wsnipex/xbmc-xvba-testing/ubuntu quantal main" >> /etc/apt/sources.list &
-rotdash2 $!
+echo "deb http://ppa.launchpad.net/wsnipex/xbmc-xvba-testing/ubuntu quantal main" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://ppa.launchpad.net/wsnipex/xbmc-xvba-testing/ubuntu quantal main" | sudo tee -a /etc/apt/sources.list
 
-sudo apt-key -y adv --keyserver keyserver.ubuntu.com --recv-keys A93CABBC > /dev/null &
-rotdash2 $!
-sudo apt-get update > /dev/null & 
-rotdash2 $!
-sudo apt-get -y dist-upgrade > /dev/null &
-rotdash2 $!
+sudo apt-key -y adv --keyserver keyserver.ubuntu.com --recv-keys A93CABBC > /dev/null
+sudo apt-get update > /dev/null
+sudo apt-get -y dist-upgrade > /dev/null
 
 echo "Wsnipex xbmc-xvba-testing PPA successfully added"
 
 ## Fix uxlaunch requirement
-sudo mkdir -p /var/lib/polkit-1/localauthority/50-local.d/ &
-rotdash2 $!
-sudo mkdir /etc/uxlaunch/ &
-rotdash2 $!
-sudo touch /etc/uxlaunch/uxlaunch &
-rotdash2 $!
+sudo mkdir -p /var/lib/polkit-1/localauthority/50-local.d
+sudo mkdir /etc/uxlaunch
+sudo touch /etc/uxlaunch/uxlaunch
 
 echo "Uxlaunch requirement fixed"
 echo "Installing lightdm (this will take a while)..."
 
 ## Install lightdm
-sudo apt-get -y install lightdm > /dev/null &
-rotdash2 $!
+sudo apt-get -y install lightdm > /dev/null
 
 echo "Lightdm successfully installed"
 echo "Installing $VIDEO_MANUFACTURER video drivers..."
 
 ## Install nvidia video drivers
-sudo apt-get -y install $VIDEO_DRIVER > /dev/null &
-rotdash2 $!
+sudo apt-get -y install $VIDEO_DRIVER > /dev/null
 
 echo "$VIDEO_MANUFACTURER video drivers successfully installed"
 echo "Rebooting..."
 
 ## Reboot
-sudo reboot now > /dev/null &
-rotdash2 $!
+sudo reboot now > /dev/null
