1b6e42e3ea3f6e798e6fe8c6cd14addf8bb33b80
Bram77 <bramvano@gmail.com>
2012-09-26 22:09:55 +0200

    Fixed a super user issue

--- 12.04/prepare_and_install_ati.sh
+++ 12.04/prepare_and_install_ati.sh
@@ -1,47 +1,50 @@
 #!/bin/sh
 
+echo ""
+echo "-----------"
+echo ""
 echo "Please enter your password to start Ubuntu preparation and XBMC installation."
 echo "Your computer will restart automatically once the process has been completed."
-sudo -s
+sudo su
 
 ## Fix locale bug
-echo "LC_MESSAGES=\"C\"" >> /etc/environment
-echo "LC_ALL=\"en_US.UTF-8\"" >> /etc/environment
+sudo echo "LC_MESSAGES=\"C\"" >> /etc/environment
+sudo echo "LC_ALL=\"en_US.UTF-8\"" >> /etc/environment
 
 echo "-----"
 echo "Locale environment bug successfully fixed"
 echo "Adding Wsnipex xbmc-xvba-testing PPA..."
 
 ## Add XBMCbuntu testing ppa to sources.list and reload repositories
-echo "" >> /etc/apt/sources.list
-echo "## XBMCbuntu testing PPA" >> /etc/apt/sources.list
-echo "deb http://ppa.launchpad.net/wsnipex/xbmc-xvba-testing/ubuntu quantal main" >> /etc/apt/sources.list
-echo "deb-src http://ppa.launchpad.net/wsnipex/xbmc-xvba-testing/ubuntu quantal main" >> /etc/apt/sources.list
+sudo echo "" >> /etc/apt/sources.list
+sudo echo "## XBMCbuntu testing PPA" >> /etc/apt/sources.list
+sudo echo "deb http://ppa.launchpad.net/wsnipex/xbmc-xvba-testing/ubuntu quantal main" >> /etc/apt/sources.list
+sudo echo "deb-src http://ppa.launchpad.net/wsnipex/xbmc-xvba-testing/ubuntu quantal main" >> /etc/apt/sources.list
 
-apt-key -y adv --keyserver keyserver.ubuntu.com --recv-keys A93CABBC > /dev/null
-apt-get update && apt-get -y dist-upgrade > /dev/null
+sudo apt-key -y adv --keyserver keyserver.ubuntu.com --recv-keys A93CABBC > /dev/null
+sudo apt-get update && apt-get -y dist-upgrade > /dev/null
 
 echo "Wsnipex xbmc-xvba-testing PPA successfully added"
 
 ## Fix uxlaunch requirement
-mkdir -p /var/lib/polkit-1/localauthority/50-local.d/
-mkdir /etc/uxlaunch/
-touch /etc/uxlaunch/uxlaunch
+sudo mkdir -p /var/lib/polkit-1/localauthority/50-local.d/
+sudo mkdir /etc/uxlaunch/
+sudo touch /etc/uxlaunch/uxlaunch
 
 echo "Uxlaunch requirement fixed"
 echo "Installing lightdm (this will take a while)..."
 
 ## Install lightdm
-apt-get -y install lightdm > /dev/null
+sudo apt-get -y install lightdm > /dev/null
 
 echo "Lightdm successfully installed"
-echo "Installing Ati video drivers..."
+echo "Installing ATI video drivers..."
 
 ## Install nvidia video drivers
-apt-get -y install fglrx > /dev/null
+sudo apt-get -y install fglrx > /dev/null
 
-echo "Ati video drivers successfully installed"
+echo "ATI video drivers successfully installed"
 echo "Rebooting..."
 
 ## Reboot
-reboot now > /dev/null
+sudo reboot now > /dev/null
