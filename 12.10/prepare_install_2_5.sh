6214f0a9e25b022db6f601f923ac7466ef7815bd
Bram77 <bram@sduo-systems.com>
2012-10-13 22:45:49 +0200

    Small changes

--- 12.10/prepare_install_2_5.sh
+++ 12.10/prepare_install_2_5.sh
@@ -2,11 +2,11 @@
 #
 # @author   Bram van Oploo
 # @date     2012-10-06
-# @version  2.4.1
+# @version  2.5.0
 #
 
 THIS_FILE=$0
-SCRIPT_VERSION="2.4.1"
+SCRIPT_VERSION="2.5.0"
 
 VIDEO_MANUFACTURER=""
 VIDEO_DRIVER=""
