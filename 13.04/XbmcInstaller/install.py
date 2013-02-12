c8a6e9cc483b64040d1d38dfc383c79b850e4b58
Bram77 <bram@sudo-systems.com>
2013-02-12 17:13:22 +0100

    Impoved file structure for v3.0.0

---
+++ 13.04/XbmcInstaller/install.py
@@ -0,0 +1,16 @@
+__author__ = "Bram van Oploo (bram@sudo-systems.com)"
+__version__ = "$Revision: 3.0.0 $"
+__date__ = "$Date: 2013/02/12 21:57:19 $"
+__copyright__ = "Copyright (c) 2013 Bram van Oploo"
+__license__ = "Creative Commons"
+
+import sys
+import os
+import stat
+import urlparse
+import VideoDrivers
+import Repositories
+import System
+import XbmcAddons
+
+print "Test"
