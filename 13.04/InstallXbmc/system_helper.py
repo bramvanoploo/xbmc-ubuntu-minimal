465b84aa57ba4120568e1764563fa7872e0177c1
Bram77 <bram@sudo-systems.com>
2013-02-18 18:19:16 +0100

    Directory and file cleanups

--- 13.04/InstallXbmc/system_helper.py
+++
@@ -1,6 +0,0 @@
-import socket
-
-def getLocalIpAddress():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("google.com", 80))
-    return s.getsockname()[0]
--- 13.04/XbmcInstaller/Database/__init__.py
+++
@@ -1,2 +0,0 @@
-import Local
-import Remote
