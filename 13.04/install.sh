22909e2352a55e56ea1c18eab31d8d587c998683
Bram77 <bram@sudo-systems.com>
2013-02-23 16:09:18 +0100

    Prepared for system tools test package

--- 13.04/install.sh
+++ 13.04/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # @author   Bram van Oploo
 # @date     2013-02-12
@@ -8,21 +8,12 @@
 clear
 
 echo "* Preparing ubuntu..."
-sudo apt-get -y install python-software-properties software-properties-common ppa-purge python python-flask lm-sensors python-apt tar wget > /dev/null 2>&1
+sudo apt-get -y install python-software-properties software-properties-common ppa-purge python python-flask python-apt tar > /dev/null 2>&1
 
-#echo "* Downloading and installing installer requirements..."
-#Download and install urwid
-#mkdir $HOME/temp > /dev/null 2>&1
-##cd $HOME/temp > /dev/null 2>&1
-#wget http://excess.org/urwid/urwid-1.1.1.tar.gz > /dev/null 2>&1
-#tar -xvzf $HOME/temp/urwid-1.1.1.tar.gz > /dev/null 2>&1
-#python $HOME/temp/urwid-1.1.1/setup.py build > /dev/null 2>&1
-#sudo python $HOME/temp/urwid-1.1.1/setup.py install > /dev/null 2>&1
-#sudo rm $HOME/temp/urwid-1.1.1.tar.gz > /dev/null 2>&1
-#sudo rm -R $HOME/temp/urwid-1.1.1 > /dev/null 2>&1
-
-#echo "* Starting installer..."
-#sudo python /media/storage_linux/Development/Personal/xbmc-ubuntu-minimal/13.04/XbmcInstaller/install.py
-
-echo "* Starting installer..."
-sudo python /media/storage_linux/Development/Personal/xbmc-ubuntu-minimal/13.04/InstallXbmc/server.py
+echo "* Downloading and installing Software package"
+cd ~
+wget https://github.com/Bram77/xbmc-ubuntu-minimal/raw/13.04_3.0.0/13.04/xbmcsystemtools.tar.gz
+# extract
+# apply init.d
+# run service
+# show message
